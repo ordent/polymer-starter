a987d17426f29c8262f4d24a2351fc7c6de5d0d4
Dimas Satrio <dimassrio@gmail.com>
2017-09-12 07:40:59 +0700

    refresh commit

--- src/scripts/config.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  window.config = {};
-  window.config.authDomain = "soca-90e15.firebaseapp.com";
-  window.config.databaseURL = "https://soca-90e15.firebaseio.com";
-  window.config.apiKey = "AIzaSyB5ORPjrvw3-3qna4m9-uPJ6lz3IYUljkE";
-  window.config.storageBucket = "";
-  window.config.messagingSenderId = "967144938427";
-  window.config.sessionName = "apps:auth:users";
-})();
